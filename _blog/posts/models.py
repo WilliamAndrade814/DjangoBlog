ac6ec35688dd46294a2d210ec0dc309671cf6673
William Andrade Machado <willianandrade76@hotmail.com>
2022-09-23 11:48:22 -0300

    progress

--- _blog/posts/models.py
+++ _blog/posts/models.py
@@ -1,4 +1,4 @@
-from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -15,9 +15,9 @@ class Post(models.Model):
     titulo = models.CharField(max_length=40)
     descricao = models.TextField(default='')
     regiao = models.ForeignKey(Region, on_delete=models.DO_NOTHING)
-    imagem = RichTextUploadingField()
+    imagem = models.TextField(default='')
     postado_em = models.DateTimeField(default=timezone.now)
-    autor = models.CharField(max_length=40)
+    autor = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     conteudo = models.TextField(default='')
 
     def __str__(self):
